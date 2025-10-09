80a6de94e41db23d5709e5d3a65b55b5c8f7dcd4
Pedro Livero <pedro_livero@hotmail.com>
2025-10-09 17:21:17 -0400

    refactor: Adding .only in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,37 @@
+// eslint.config.mjs
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import cypress from "eslint-plugin-cypress";
+import noOnlyTests from "eslint-plugin-no-only-tests";
 
 export default defineConfig([
+  js.configs.recommended,
+
   {
     files: ["**/*.{js,mjs,cjs}"],
+
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
+        ...globals.mocha,
+        cy: "readonly",
+        Cypress: "readonly",
       },
     },
-    plugins: ["cypress"],
-    extends: ["eslint:recommended", "plugin:cypress/recommended"],
+
+    plugins: {
+      cypress,
+      "no-only-tests": noOnlyTests,
+    },
+
     rules: {
+      ...cypress.configs.recommended.rules,
+
       semi: ["error", "always"],
       quotes: ["error", "double"],
+      "no-only-tests/no-only-tests": "error",
     },
   },
 ]);
