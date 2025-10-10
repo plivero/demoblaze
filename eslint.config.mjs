8e174510c77eeb5748f8b7ad3f841f6219368974
Pedro Livero <pedro_livero@hotmail.com>
2025-10-10 17:05:44 -0400

    main: Pipeline fix1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
 // eslint.config.mjs
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
 import cypress from "eslint-plugin-cypress";
 import noOnlyTests from "eslint-plugin-no-only-tests";
 
 export default defineConfig([
-  js.configs.recommended,
-
   {
     files: ["**/*.{js,mjs,cjs}"],
-
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -18,17 +14,16 @@ export default defineConfig([
         ...globals.mocha,
         cy: "readonly",
         Cypress: "readonly",
+        expect: "readonly",
+        assert: "readonly",
       },
     },
-
     plugins: {
       cypress,
       "no-only-tests": noOnlyTests,
     },
-
+    extends: ["eslint:recommended", "plugin:cypress/recommended"],
     rules: {
-      ...cypress.configs.recommended.rules,
-
       semi: ["error", "always"],
       quotes: ["error", "double"],
       "no-only-tests/no-only-tests": "error",
